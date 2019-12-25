599e748f008c6a7ee932ba5752a0f5405279259a
1361356742@qq.vom <1361356742@qq.com>
2019-12-25 17:58:51 +0800

    网络图片；

--- lib/NewRoute.dart
+++ lib/NewRoute.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 class NewRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
       appBar: AppBar(
         title: Text("文本、字体样式"),
@@ -16,16 +15,40 @@ class NewRoute extends StatelessWidget {
                 EdgeInsets.only(left: 0.0, top: 16.0, right: 0.0, bottom: 0.0),
           ),
           FlatButton(
+            textColor: Colors.white,
+            color: Colors.blue,
             child: Text("This is new route" + text),
+            //设置圆角
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0)),
             onPressed: () {
               Navigator.push(context, MaterialPageRoute(builder: (context) {
                 return NewRoute(text: "我得页面B传的值");
               }));
             },
           ),
+          RaisedButton(
+            elevation: 5,
+            //这个默认有阴影
+            textColor: Colors.white,
+            color: Colors.blue,
+            child: Text("This is new route" + text),
+            //设置圆角
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0)),
+            onPressed: () {
+              print("按下");
+            },
+          ),
+
           new Image.asset('my_icons/all.png'),
           new Image(
             image: new AssetImage('my_icons/all.png'),
+          ),
+          //网络图片
+          Image.network(
+            "http://dmimg.5054399.com/allimg/pkm/pk/22.jpg",
+            width: 100,
           ),
 //          设置padding
           Padding(
